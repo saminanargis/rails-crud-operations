d3e0f71025b039c8a8a89fc6ea052fac8c51dcc2
Samina Nargis <samina.nargis01@gmail.com>
2022-03-30 12:49:48 +0500

    CURD operations

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,42 @@
+class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+  def new
+    @employee = Employee.new
+  end
+  def show
+    @employee = Employee.find(params[:id])
+  end
+  def create
+    @employee = Employee.new(employee_params)
+
+    if @employee.save
+      redirect_to @employee
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    if @employee.update(employee_params)
+      redirect_to @employee
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+    redirect_to root_path, status: :see_other
+  end
+  def employee_params
+    params.require(:employee).permit(:name, :job, :department, :salary, :hire_date)
+  end
+end
