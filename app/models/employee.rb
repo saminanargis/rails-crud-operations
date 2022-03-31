deb475579e3132991baaf16257538d31b57f7ec1
Samina Nargis <samina.nargis01@gmail.com>
2022-03-31 05:58:12 +0500

    add validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+  validates :name, length: { minimum: 10 }
+  validates :job, length: { minimum: 15 }
+  validates :department, length: { in: 15..50 }
+  validates :salary, numericality: {greater_than_or_equal_to: 0}
+  validates :hire_date, presence: true
 end
